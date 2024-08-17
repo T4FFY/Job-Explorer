2af50c04b3fbd126004223ff6f0dfbc69e3a0d52
Zafina <zafinavictory@gmail.com>
2024-08-17 11:49:39 -0400

    Adding React + tailwind

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import MyComponent from "./components/MyComponent";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500">Hi there</h1>
+      <h1 className="text-green-500">Hello there</h1>
+      <MyComponent></MyComponent>
+      <MyComponent></MyComponent>
+      <MyComponent></MyComponent>
+      <MyComponent></MyComponent>
+      <MyComponent></MyComponent>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/MyComponent.jsx
@@ -0,0 +1,11 @@
+function MyComponent() {
+  return (
+    <>
+      <p>this is MyComponent</p>
+      <p>second p in my MyComponent</p>
+      <br />
+    </>
+  );
+}
+
+export default MyComponent;
