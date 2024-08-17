f16042726a38d3b5eccc58624e849ef3614b03c3
Zafina <zafinavictory@gmail.com>
2024-08-17 15:53:12 -0400

    Fixing menu

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/pricing" element={<Pricing />} />
-          <Route path="/menu" element={<Menu />} />
         </Routes>
       </div>
   </> 
