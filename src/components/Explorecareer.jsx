8fc2d4739e970cacee1478319bf2bfd398c3a845
phuonganhdangamy <anh.dangphuong@mail.utoronto.ca>
2024-08-18 11:54:50 +0700

    change images on Explorecareer

--- src/components/Explorecareer.jsx
+++ src/components/Explorecareer.jsx
@@ -21,68 +21,68 @@ const jobAreas = [
   {
       id: 1,
       title: "Technology and IT",
-      imageUrl: "https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80",
+      imageUrl: "https://www.shutterstock.com/shutterstock/photos/2136788117/display_1500/stock-photo-late-at-night-in-private-office-male-businessman-works-on-a-laptop-computer-he-look-at-the-camera-2136788117.jpg",
       learnMore: "/technology-and-it"
   },
   {
       id: 2,
       title: "Arts and Media",
-      imageUrl: "https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80",
+      imageUrl: "https://media.gettyimages.com/id/1399942653/photo/artist-talking-to-the-press.jpg?s=612x612&w=gi&k=20&c=GPrRbVfQ3s3282Txx8FDSVUcab1p-zPZuI1D_-s6JK4=",
       learnMore: "/arts-and-media"
   },
   // Add more job areas here
   {
       id: 3,
       title: "Science and Engineering",
-      imageUrl: "https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80",
+      imageUrl: "https://st4.depositphotos.com/1971587/31275/i/450/depositphotos_312755238-stock-photo-electronic-laboratory-working-process.jpg",
       learnMore: "/science-and-engineering"
     },
   {
       id: 4,
       title: "Healthcare and Medicine",
-      imageUrl: "https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80",
+      imageUrl: "https://www.shutterstock.com/image-photo/happy-woman-doctor-on-tablet-600nw-2273374687.jpg",
       learnMore: "/healthcare-and-medicine"
     },
   {
       id: 5,
       title: "Education and Training",
-      imageUrl: "https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80",
+      imageUrl: "https://media.istockphoto.com/id/1312139041/photo/learning-on-the-job.jpg?s=612x612&w=0&k=20&c=KmGoZhp92ukKgYCtxulX9yViM1aQTtYtq-pvLr7Q2TA=",
       learnMore: "/education-and-training"
     },
   {
       id: 6,
       title: "Business and Finance",
-      imageUrl: "https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80",
+      imageUrl: "https://img.freepik.com/free-photo/group-asian-business-people-meeting-about-business-plan-present-explain-stock-financial_74952-2970.jpg",
       learnMore: "/business-and-finance"
     },
   {
       id: 7,
       title: "Social Services and Community Work",
-      imageUrl: "https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80",
+      imageUrl: "https://www.shutterstock.com/image-photo/black-man-volunteering-giving-box-600nw-2256153733.jpg",
       learnMore: "/social-services-and-community-work"
     },
   {
       id: 8,
       title: "Skilled Trades and Crafts",
-      imageUrl: "https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80",
+      imageUrl: "https://www.shutterstock.com/image-photo/two-artisans-welder-their-craft-260nw-1312181897.jpg",
       learnMore: "/skilled-trades-and-crafts"
     },
   {
       id: 9,
       title: "Sports and Recreation",
-      imageUrl: "https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80",
+      imageUrl: "https://thumbs.dreamstime.com/b/sports-athlete-black-woman-job-outdoor-break-nature-beach-fitness-runner-ocean-exercise-sports-athlete-327905386.jpg",
       learnMore: "/sports-and-recreation"
     },
   {
       id: 10,
       title: "Environmental and Agricultural",
-      imageUrl: "https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80",
+      imageUrl: "https://www.shutterstock.com/image-photo/farmer-girl-harvests-turniprooted-cabbage-600nw-2400029675.jpg",
       learnMore: "/environmental-and-agricultural"
     },
   {
       id: 11,
       title: "Law and Public Safety",
-      imageUrl: "https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80",
+      imageUrl: "https://media.istockphoto.com/id/483557301/photo/police-officers-on-duty.jpg?s=612x612&w=0&k=20&c=lqpRUJ4YbNqB247QUosZxq0ErIKogYli0XAZPiLkdZs=",
       learnMore: "/law-and-public-safety"
     },
 ];
